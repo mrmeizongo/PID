83aafdd5d6ce7e84632271090deb81db8d2fc15f
Jamal Meizongo <mrmeizongo@outlook.com>
2024-09-02 14:03:38 -0400

    Added integrator max

--- src/PID.h
+++ src/PID.h
@@ -1,26 +1,31 @@
-/*
-MIT License
+/* ============================================
+Copyright (C) 2024 Jamal Meizongo
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-Copyright(c) 2024 Jamal Meizongo
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-    Permission is hereby granted,
-    free of charge, to any person obtaining a copy of this software and associated documentation files(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and / or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions :
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
 
-    The above copyright notice and this permission notice shall be included in all copies
-    or
-    substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS",
-    WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-    DAMAGES OR OTHER
-    LIABILITY,
-    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+===============================================
 */
 #pragma once
+
+#ifndef _PID_H
+#define _PID_H
 #include <inttypes.h>
 
 class PID
@@ -29,15 +34,17 @@ public:
     PID();                           // Empty Constructor
     PID(float, float, float, float); // Constructor with initialization parameters
     void ResetI(void);               // Reset PID integrator
-    float Compute(float);            // Generate the PID output to be added to the servo
+    int16_t Compute(float);          // Generate the PID output to be added to the servo
 
     float getKp(void) { return Kp; }
     float getKi(void) { return Ki; }
     float getKd(void) { return Kd; }
+    float getIMax(void) { return IMax; }
 
     void setKp(float _Kp) { Kp = _Kp; }
     void setKi(float _Ki) { Ki = _Ki; }
     void setKd(float _Kd) { Kd = _Kd; }
+    void setIMax(float _IMax) { IMax = _IMax; }
 
 private:
     float Kp;
@@ -57,3 +64,4 @@ private:
     float previousDerivative; // for low-pass filter calculation
     unsigned long previousTime;
 };
+#endif //_PID_H
